ba777a8a11ba8c8add8a545bedc0a9f315e4599f
Zidane Innis <zidaneinnis@gmail.com>
2023-06-19 15:49:16 -0500

    Using Stubs Across Tests

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
-	it('should fetch the user location', () => {
+	beforeEach(() => {
 		cy.visit('/').then((win) => {
 			cy.stub(win.navigator.geolocation, 'getCurrentPosition')
 				.as('getUserPosition')
@@ -11,9 +11,16 @@ describe('share location', () => {
 					}, 100);
 				});
 		});
+	});
+	it('should fetch the user location', () => {
 		cy.get('[data-cy="get-loc-btn"]').click();
 		cy.get('@getUserPosition').should('have.been.called');
 		cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
 		cy.get('[data-cy="actions"]').should('contain', 'Location fetched');
+	});
+
+	it('should share a location URL', () => {
+		cy.get('[data-cy="name-input"]').type('Jon Zherka');
+		cy.get('[data-cy="get-loc-btn"]').click();
 	});
 });
